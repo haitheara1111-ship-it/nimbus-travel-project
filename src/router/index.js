ed301f0e75dd00f6219d8d4002a4f77e53abb6c0
haitheara <haitheara1111@gmail.com>
2026-01-28 14:12:14 +0700

    feat: update routing and views for login page, add contact form and remove unused components

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import TestView from '../views/TestView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/test',
+      name: 'test',
+      component: TestView,
+    },
+    {
+      path: '/Home',
+      name: 'Home',
+      component: HomeView,
+    },
+  ],
 })
 
 export default router
