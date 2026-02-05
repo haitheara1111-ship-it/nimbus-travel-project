450e7cf48140d2769fc0bfa5bfe98625e4b23c39
Rith0068 <sothearithkeom@gmail.com>
2026-02-05 09:17:39 +0700

    ths page 7 day is allready done but not  take api

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DayView from '@/views/DayView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/day',
+      name: 'day',
+      component: DayView,
+    },
+  ],
 })
 
 export default router
